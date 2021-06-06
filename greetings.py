9f1a2ef0a075f15937fd7258767e38dbb42eff8d
Oswaldo Yanez <oswaldo.business31@gmail.com>
2021-06-06 12:46:04 -0400

    Python file

---
+++ greetings.py
@@ -0,0 +1,7 @@
+def greeting(user):
+    return (f"hello {user}, welcome to our system")
+
+if __name__ == '__main__':
+    user = input("Enter your username : ").capitalize()
+    greeting1 = greeting(user)
+    print(greeting1)
